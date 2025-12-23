8df69ee635e9cca7f8c9a93a081c4ae338dce776
Niwely <hello@is-a.space>
2025-12-23 11:24:09 +0300

    Update bot.py

--- petabytestorage/bot.py
+++ petabytestorage/bot.py
@@ -11,7 +11,7 @@ import discord
 from tortoise.exceptions import DoesNotExist
 from .models import File
 from .globals import logger
-from .utils.crypto import encrypt, decrypt
+from .utils.crypto import compress_and_encrypt, decrypt_and_decompress
 from . import config
 
 class UploadBot:
@@ -42,6 +42,7 @@ class UploadBot:
         self.iops_lock = asyncio.Lock()
         self.current_read_ahead_amount = self.accel_start
         self.ready_event = asyncio.Event()
+        self.read_semaphore = asyncio.Semaphore(config.Cache.MAX_CONCURRENT_READS)
     
     def _get_cache_key(self, file_id: Any, chunk_idx: int) -> str:
         return f"{str(file_id)}__{chunk_idx}"
@@ -74,24 +75,30 @@ class UploadBot:
         await self.upload_queue.join()
 
     async def _fetch_chunk_from_source(self, file: File, chunk_idx: int) -> bytes:
-        chunk_path = self._chunk_cache_path(str(file.id), chunk_idx)
-        if os.path.exists(chunk_path):
-            with open(chunk_path, "rb") as f: data = decrypt(f.read())
-            if len(data) < self.chunk_size: data += b'\x00' * (self.chunk_size - len(data))
-            return data
-        
-        await self.ready_event.wait()
-        cmeta = file.chunks.get(str(chunk_idx))
-        if cmeta and cmeta.get("msg_id"):
-            try:
-                msg = await self.channel.fetch_message(cmeta["msg_id"])
-                enc = await msg.attachments[0].read()
-                data = decrypt(enc)
-                if len(data) < self.chunk_size: data += b'\x00' * (self.chunk_size - len(data))
-                return data
-            except discord.NotFound:
-                logger.warning(f"Message for chunk {chunk_idx} of file {file.id} not found. Returning sparse data.")
-        return b'\x00' * self.chunk_size
+        async with self.read_semaphore:
+            chunk_path = self._chunk_cache_path(str(file.id), chunk_idx)
+            raw_data = b''
+            
+            if os.path.exists(chunk_path):
+                with open(chunk_path, "rb") as f:
+                    raw_data = decrypt_and_decompress(f.read())
+            else:
+                await self.ready_event.wait()
+                cmeta = file.chunks.get(str(chunk_idx))
+                if cmeta and cmeta.get("msg_id"):
+                    try:
+                        msg = await self.channel.fetch_message(cmeta["msg_id"])
+                        enc_data = await msg.attachments[0].read()
+                        raw_data = decrypt_and_decompress(enc_data)
+                    except discord.NotFound:
+                        logger.warning(f"Message for chunk {chunk_idx} of file {file.id} not found. Returning sparse data.")
+            
+            if raw_data and len(raw_data) < self.chunk_size:
+                raw_data += b'\x00' * (self.chunk_size - len(raw_data))
+            elif not raw_data:
+                raw_data = b'\x00' * self.chunk_size
+            
+            return raw_data
 
     async def _prime_file_cache(self, file: File):
         if file.id in self.primed_files: return
@@ -130,9 +137,12 @@ class UploadBot:
         if cache_key in self.read_ahead_cache:
             self.read_ahead_cache.move_to_end(cache_key)
             return self.read_ahead_cache[cache_key]
+        
         chunk_data = await self._fetch_chunk_from_source(file, chunk_idx)
+        
         if len(self.read_ahead_cache) >= self.cache_limit: self.read_ahead_cache.popitem(last=False)
         self.read_ahead_cache[cache_key] = chunk_data
+        
         asyncio.create_task(self._populate_read_ahead_cache(file, chunk_idx))
         return chunk_data
 
@@ -154,17 +164,25 @@ class UploadBot:
         start_byte, end_byte = download_range
         if not file.size or start_byte >= file.size: return b""
         if end_byte >= file.size: end_byte = file.size - 1
-        start_chunk = start_byte // self.chunk_size
-        end_chunk = end_byte // self.chunk_size
-        buf = io.BytesIO()
-        for i in range(start_chunk, end_chunk + 1):
-            chunk_data = await self._read_chunk_from_cache(file, i)
-            start_in_chunk = 0
-            if i == start_chunk: start_in_chunk = start_byte % self.chunk_size
-            end_in_chunk = self.chunk_size
-            if i == end_chunk: end_in_chunk = (end_byte % self.chunk_size) + 1
-            buf.write(chunk_data[start_in_chunk:end_in_chunk])
-        return buf.getvalue()
+        
+        start_chunk_idx = start_byte // self.chunk_size
+        end_chunk_idx = end_byte // self.chunk_size
+        
+        chunk_indices_to_fetch = range(start_chunk_idx, end_chunk_idx + 1)
+        tasks = [self._read_chunk_from_cache(file, i) for i in chunk_indices_to_fetch]
+        all_chunk_data = await asyncio.gather(*tasks)
+
+        start_in_chunk = start_byte % self.chunk_size
+        end_in_chunk = (end_byte % self.chunk_size) + 1
+        
+        if start_chunk_idx == end_chunk_idx:
+            return all_chunk_data[0][start_in_chunk:end_in_chunk]
+        
+        first_chunk = all_chunk_data[0][start_in_chunk:]
+        middle_chunks = all_chunk_data[1:-1]
+        last_chunk = all_chunk_data[-1][:end_in_chunk]
+        
+        return first_chunk + b"".join(middle_chunks) + last_chunk
 
     async def discord_patch(self, file: File, start: int, buf: bytes):
         start_chunk = start // self.chunk_size
@@ -179,11 +197,15 @@ class UploadBot:
             len_to_write = patch_end - patch_start
             data_slice = buf[buf_ptr : buf_ptr + len_to_write]
             chunk_data[patch_start : patch_start + len(data_slice)] = data_slice
+            
+            decrypted_chunk_bytes = bytes(chunk_data)
             chunk_path = self._chunk_cache_path(str(file.id), i)
-            with open(chunk_path, "wb") as f: f.write(encrypt(chunk_data))
+            with open(chunk_path, "wb") as f: f.write(compress_and_encrypt(decrypted_chunk_bytes))
+
             cache_key = self._get_cache_key(file.id, i)
             if len(self.write_cache) >= self.cache_limit: self.write_cache.popitem(last=False)
-            self.write_cache[cache_key] = bytes(chunk_data)
+            self.write_cache[cache_key] = decrypted_chunk_bytes
+            
             await self.upload_queue.put((str(file.id), i))
             buf_ptr += len_to_write
 
@@ -228,8 +250,7 @@ class UploadBot:
                 if msg:
                     try:
                         file = await File.get(id=UUID(file_id_str))
-                        unencrypted_size = len(decrypt(enc_data))
-                        file.chunks[str(chunk_idx)] = {"msg_id": msg.id, "size": unencrypted_size}
+                        file.chunks[str(chunk_idx)] = {"msg_id": msg.id}
                         await file.save(update_fields=['chunks'])
                         os.remove(chunk_path)
                     except DoesNotExist:
