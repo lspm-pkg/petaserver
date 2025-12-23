2aa7310f56d1c471ef6771c59f9ea88b44e64aa1
Niwely <hello@is-a.space>
2025-12-23 14:01:44 +0300

    Compression is worthless kinda

--- petabytestorage/utils/crypto.py
+++ petabytestorage/utils/crypto.py
@@ -1,7 +1,6 @@
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 from ..config import ENCRYPTION_KEY
 import secrets
-import zstandard as zstd
 
 def encrypt(chunk: bytes) -> bytes:
     nonce = secrets.token_bytes(12)
@@ -9,11 +8,3 @@ def encrypt(chunk: bytes) -> bytes:
 
 def decrypt(chunk: bytes) -> bytes:
     return AESGCM(ENCRYPTION_KEY).decrypt(chunk[:12], chunk[12:], b"")
-
-def compress_and_encrypt(chunk: bytes) -> bytes:
-    compressed_data = zstd.compress(chunk)
-    return encrypt(compressed_data)
-
-def decrypt_and_decompress(chunk: bytes) -> bytes:
-    decrypted_data = decrypt(chunk)
-    return zstd.decompress(decrypted_data)
